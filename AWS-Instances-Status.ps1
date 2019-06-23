367d8bdd6b21e8243db52cc67241cd032115eaa8
Raja Pamuluri <32969911+raja-converged@users.noreply.github.com>
2019-06-23 14:58:22 -0500

    First file

---
+++ AWS-Instances-Status.ps1
@@ -0,0 +1,5 @@
+﻿<# This script will give the status of all yoru instances running in yoru AWS account. It will give with the instanceID and before running this make sure you connect to AWS account using
+﻿your AccessKey and SecretKey and right permissions to access the EC2 instances.#>
+﻿foreach ($i in ((Get-EC2Instance).instances |select InstanceId)) { (Get-EC2InstanceStatus -InstanceId $i.InstanceId).status |select Details,Status,@{Name ="Instance ID";expr = {$i.InstanceId}} }
+﻿foreach ($i in ((Get-EC2Instance).instances |select InstanceId)) { (Get-EC2InstanceStatus -InstanceId $i.InstanceId).systemstatus |select Details,Status,@{Name ="Instance ID";expr = {$i.InstanceId}} }
+﻿
